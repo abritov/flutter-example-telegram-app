3efe4befc4a7dd2f0dea6c71048032f871fc8a89
Alexander Britov <pkgsail.dev@gmail.com>
2019-06-18 18:11:34 +0300

    fix(ui): SettingsPage -> Cupertino

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:my_ui_example_1/widgets/search.dart';
@@ -141,67 +142,48 @@ class SettingsPage extends StatelessWidget {
       )
     ].map((tile) => _tileBorderContainer(tile, divideBorderColor));
 
-    return CustomScrollView(controller: _scrollController, slivers: [
-      SliverAppBar(
-          pinned: true,
-          elevation: 0.0,
-          backgroundColor: Color(0xf7f7f7f7),
-          title: Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: Text('Settings',
-                style: TextStyle(color: Colors.black, fontSize: 17.0)),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.edit),
-              color: Color(0xFF5E92D7),
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              onPressed: () {},
-            ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: Size(1.0, 1.0),
-            child: Container(
-                decoration:
-                    BoxDecoration(border: Border(bottom: borderSideDivider))),
-          )),
-      SliverList(
-        delegate: SliverChildListDelegate([
-          SearchBoxWidget(),
-          Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                      bottom: borderSideDivider, top: borderSideDivider)),
-              height: 90.0,
-              child: ListTile(
-                leading: CircleAvatar(
-                  child: Text("Me"),
-                  radius: 30.0,
-                ),
-                title: Padding(
-                  padding: EdgeInsets.only(top: 12.0),
-                  child: Text("Alexander",
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.w500)),
-                ),
-                subtitle: Padding(
-                    padding: EdgeInsets.only(top: 2.0),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("+7 999 999-99-99",
-                              style: TextStyle(fontSize: 16.0)),
-                          Text("@telegram", style: TextStyle(fontSize: 16.0))
-                        ])),
-                trailing: IconButton(
-                  icon: Icon(Icons.arrow_forward_ios),
-                  iconSize: 15.0,
-                ),
-              )),
-        ]..addAll(entries)),
-      )
-    ]);
+    return Material(
+      child: CustomScrollView(controller: _scrollController, slivers: [
+        CupertinoSliverNavigationBar(
+          largeTitle: SearchBoxWidget(),
+          backgroundColor: Colors.grey[100],
+          middle: Text("Settings"),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(bottom: borderSideDivider)),
+                height: 90.0,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text("Me"),
+                    radius: 30.0,
+                  ),
+                  title: Padding(
+                    padding: EdgeInsets.only(top: 12.0),
+                    child: Text("Alexander",
+                        style: TextStyle(
+                            fontSize: 18.0, fontWeight: FontWeight.w500)),
+                  ),
+                  subtitle: Padding(
+                      padding: EdgeInsets.only(top: 2.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("+7 999 999-99-99",
+                                style: TextStyle(fontSize: 16.0)),
+                            Text("@telegram", style: TextStyle(fontSize: 16.0))
+                          ])),
+                  trailing: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios),
+                    iconSize: 15.0,
+                  ),
+                )),
+          ]..addAll(entries)),
+        )
+      ]),
+    );
   }
 }

e7e052c64548f91b9e8ac702fe96af2b42bf2c74
Alexander Britov <pkgsail.dev@gmail.com>
2019-06-18 18:17:46 +0300

    fix(ui): ChatPage -> Cupertino

--- lib/widgets/chat.dart
+++ lib/widgets/chat.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:my_ui_example_1/models/chat.dart';
@@ -35,7 +36,7 @@ class ChatPage extends StatelessWidget {
   List<ChatModel> chats;
 
   ScrollController _scrollController =
-      ScrollController(initialScrollOffset: 55.2);
+      ScrollController(initialScrollOffset: 55.0);
 
   Widget _tileBorderContainer(Widget tile, Color borderColor) {
     final _borderSideDivider = BorderSide(width: 1.0, color: borderColor);
@@ -49,42 +50,33 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final divideBorderColor = Theme.of(context).dividerColor;
-    final borderSideDivider = BorderSide(width: 1.0, color: divideBorderColor);
-    return CustomScrollView(controller: _scrollController, slivers: [
-      SliverAppBar(
-          pinned: true,
-          elevation: 0.0,
-          backgroundColor: Color(0xf7f7f7f7),
-          title: Padding(
-            padding: EdgeInsets.only(top: 8.0),
-            child: Text('Chats',
-                style: TextStyle(color: Colors.black, fontSize: 17.0)),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.edit),
-              color: Color(0xFF5E92D7),
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
+    // final borderSideDivider = BorderSide(width: 1.0, color: divideBorderColor);
+    return Material(
+      child: CupertinoPageScaffold(
+        child: CustomScrollView(controller: _scrollController, slivers: [
+          CupertinoSliverNavigationBar(
+            middle: Text("Chats"),
+            backgroundColor: Colors.grey[100],
+            largeTitle: SearchBoxWidget(hint: "Search for messages or users"),
+            leading: CupertinoButton(
+              padding: EdgeInsets.only(bottom: 0.0),
               onPressed: () {},
+              child: Text("Edit"),
             ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: Size(1.0, 1.0),
-            child: Container(
-                decoration:
-                    BoxDecoration(border: Border(bottom: borderSideDivider))),
-          )),
-      SliverList(
-        delegate: SliverChildListDelegate(
-            [SearchBoxWidget(hint: "Search for messages or users")]),
+            trailing: IconButton(
+              icon: IconTheme(
+                  data: IconThemeData(color: CupertinoColors.activeBlue),
+                  child: Icon(CupertinoIcons.create)),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(chats
+                .map((chat) =>
+                    _tileBorderContainer(ChatWidget(chat), divideBorderColor))
+                .toList()),
+          )
+        ]),
       ),
-      SliverList(
-        delegate: SliverChildListDelegate(chats
-            .map((chat) =>
-                _tileBorderContainer(ChatWidget(chat), divideBorderColor))
-            .toList()),
-      )
-    ]);
+    );
   }
 }
